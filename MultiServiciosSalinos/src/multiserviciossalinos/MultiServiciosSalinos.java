2205d94d47d1c47939b8f4c4a0b7f505134da162
joel verde <jverde241@unab.edu.pe>
2025-01-03 13:12:46 +0100

    creando un proyecto java

---
+++ MultiServiciosSalinos/src/multiserviciossalinos/MultiServiciosSalinos.java
@@ -0,0 +1,16 @@
+package multiserviciossalinos;
+
+/**
+ *
+ * @author nanci
+ */
+public class MultiServiciosSalinos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
