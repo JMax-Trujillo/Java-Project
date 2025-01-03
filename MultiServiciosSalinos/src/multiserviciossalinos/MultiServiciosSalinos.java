a33108afcefddca12a9e766cf3f44c2c90e935e4
joel verde <jverde241@unab.edu.pe>
2025-01-03 13:41:19 +0100

    nuevo cambio

--- MultiServiciosSalinos/src/multiserviciossalinos/MultiServiciosSalinos.java
+++ MultiServiciosSalinos/src/multiserviciossalinos/MultiServiciosSalinos.java
@@ -6,9 +6,8 @@ public class MultiServiciosSalinos {
         Scanner object=new Scanner(System.in);
         System.out.println("digita tu nombre: ");
         String nombre=object.nextLine();
-        System.out.println("tu nombre es " + nombre);
-        // TODO code application logic here
-        System.out.println("Hola Mundo");
+        System.out.println("hola genteeeee");
+                
     }
     
 }
