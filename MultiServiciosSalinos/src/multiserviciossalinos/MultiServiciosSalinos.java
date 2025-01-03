4a54fd33f01de1b3ac72a405d0570f31c9a37d31
JMax-Trujillo <JeanMax052025@gmail.com>
2025-01-03 08:55:06 -0400

    quitando comentarios

--- MultiServiciosSalinos/src/multiserviciossalinos/MultiServiciosSalinos.java
+++ MultiServiciosSalinos/src/multiserviciossalinos/MultiServiciosSalinos.java
@@ -1,14 +1,7 @@
 package multiserviciossalinos;
-import java.util.Scanner;
-import static multiserviciossalinos.Control_Inventario.ejecutable1;
-public class MultiServiciosSalinos {
 
+public class MultiServiciosSalinos {
     public static void main(String[] args) {
-        Scanner object=new Scanner(System.in);
-        System.out.println("digita tu nombre: ");
-        String nombre=object.nextLine();
-        System.out.println("hola genteeeee");
-        ejecutable1();
+        
     }
-    
 }
--- MultiServiciosSalinos/src/multiserviciossalinos/PROBLEMA4.java
+++ MultiServiciosSalinos/src/multiserviciossalinos/PROBLEMA4.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package multiserviciossalinos;
 
-/**
- *
- * @author nanci
- */
 public class PROBLEMA4 {
     public static void ejecutable4(){
         // Kevin
